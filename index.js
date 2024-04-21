d2a54c0c6980ad39e50177d27c6d1d570c9ef7ee
SugrivLodhi <sugrivmlvt@gmail.com>
2024-04-21 23:58:11 +0530

    resolve cors error

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import dbConection from './utils/dbConnection.js'
 import { registerUser, userLogin } from './controller/user.js'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 const app = express()
 const port = 8080
 dbConection().then((v)=>{
   console.log("db connected")
-}).catch((v)=>{
-console.log("failed to connect")
+}).catch((err)=>{
+console.log("failed to connects",err)
 })
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());   
 app.use(cookieParser())
@@ -19,5 +21,5 @@ app.get('/',(req,res)=>{
 app.post('/register',registerUser)
 app.post('/login',userLogin)
 app.listen(port,()=>{
-    console.log("listen at port 8000")
+    console.log(`listen at port ${port}`)
 })
